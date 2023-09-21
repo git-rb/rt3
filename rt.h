4e0c2924814d94e41579088974792a2e4b87c470
Robert Blanch <rblanch@gmail.com>
2023-09-20 19:18:35 -0500

    additions

--- rt.h
+++ rt.h
@@ -3,9 +3,26 @@
 
 namespace rt {
 
+	bool appx_equal(double a, double b) {
+		return std::abs(a-b) < 0.00001;
+	}
+
 	struct tuple {
 		double x, y, z, w; 
 	};
+
+	bool operator== (tuple const& a, tuple const& b) {
+		return appx_equal(a.x, b.x) &&
+			appx_equal(a.y, b.y) &&
+			appx_equal(a.z, b.z) &&
+			appx_equal(a.w, b.w);
+	}
+	auto operator+ (tuple const& a, tuple const& b) {
+		return tuple {a.x + b.x, a.y + b.y, a.z + b.z, a.w + b.w};
+	}
+	auto operator- (tuple const& a, tuple const& b) {
+		return tuple {a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w};
+	}
 
 	bool is_point(tuple const& t) {
 		return t.w == 1.0;
@@ -13,6 +30,13 @@ namespace rt {
 	bool is_vector(tuple const& t) {
 		return t.w == 0.0;
 	}
+	auto point(double x, double y, double z) {
+		return tuple {x,y,z,1.0};
+	}
+	auto vector(double x, double y, double z) {
+		return tuple {x,y,z,0.0};
+	}
+
 }
 
 #endif
--- test_rt.cpp
+++ test_rt.cpp
@@ -7,24 +7,45 @@
 #include "rt.h"
 #include "simtest.h"
 
-sim_test::sl loc;
 
 int main() {
 	using namespace rt;
 	using namespace sim_test;
 
 	test("A tuple with w=1.0 is a point", [] {
-				auto a = tuple {4.3, -4.2, 3.1, 1.0};
-				check(a.x == 4.3);
-				check(a.y == -4.2);
-				check(a.z == 3.1);
-				check(a.w == 1.0);
-				check(is_point(a));
-			});
+			auto a = tuple {4.3, -4.2, 3.1, 1.0};
+			check(a.x == 4.3);
+			check(a.y == -4.2);
+			check(a.z == 3.1);
+			check(a.w == 1.0);
+			check(is_point(a));
+		});
 
-	test("a tuple with w=0.0 is a point", [] {
-				auto a = tuple {4.3, -4.2, 3.1, 0.0};
-				check(is_vector(a));
-			});
+	test("a tuple with w=0.0 is a vector", [] {
+			auto a = tuple {4.3, -4.2, 3.1, 0.0};
+			check(is_vector(a));
+		});
+
+	test("point() creates tuples with w=1.0", [] {
+			auto p = point(4, -4, 3);
+			check(p == tuple {4, -4, 3, 1});
+		});
+
+	test("vector() creates tuple with w=0.0", [] {
+			auto v = vector(4, -4, 3);
+			check(v == tuple {4, -4, 3, 0});
+		});
+	
+	test("adding tuples", [] {
+			auto t1 = tuple(3, -2, 5, 1);
+			auto t2 = tuple(-2, 3, 1, 0);
+			check(t1 + t2 == tuple(1, 1, 6, 1));
+		});
+
+	test("Subtracting two points", [] {
+			auto p1 = point(3, 2, 1);
+			auto p2 = point(5, 6, 7);
+			check(p1 - p2 == vector(-2, -4, -6));
+		});
 
 }
